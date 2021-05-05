4e99d861c97144bb7e1afc1ad001090e01c87fdf
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2021-05-05 03:45:13 -0700

    DBMS Project

---
+++ Ecommerce Site PHP/ecommerce/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>DBMS MINI PROJECT </b>
+    </div>
+    <strong>Simple E-Commerce : </strong>
+</footer>
---
+++ Ecommerce Site PHP/ecommerce/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>DBMS MINI PROJECT</b>
+      </div>
+      <strong>Simple E-Commerce : </a></strong>
+    </div>
+</footer>
